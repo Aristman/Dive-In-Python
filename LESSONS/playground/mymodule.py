1893532e9ca9743911a084e076818a6c729927fb
Aristman <armobila@yandex.ru>
2020-03-11 22:08:36 +0300

    Work

---
+++ LESSONS/playground/mymodule.py
@@ -0,0 +1,9 @@
+import sys
+import mypackage
+from mypackage.utils import multiply
+
+print(multiply(4,5))
+# print('\n'.join(sys.path))
+if __name__ == '__main__':
+    print('Basic launch')
+print(mypackage)
---
+++ LESSONS/playground/mypackage/__init__.py
@@ -0,0 +1,1 @@
+print('Init string from my package')
