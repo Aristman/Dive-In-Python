52624fb5c6d7fc7e2d8280638273b3f29ec0c34d
Aristman <armobila@yandex.ru>
2020-03-14 20:25:12 +0300

    Work WINDOWS 7 64

---
+++ LESSONS/CityWeather/city.py
@@ -0,0 +1,41 @@
+import pprint
+import requests
+from dateutil.parser import parse
+import yandex_weather_api
+
+
+class YandexWeatherForecast:
+
+    def get(self, city):
+        lat, lon = '53.894548', '30.330654'
+        API_key = '1539ef79-053b-4013-9bea-4b78ec83fe3d'
+        url = f'GET https://api.weather.yandex.ru/v1/informers?'
+        data = yandex_weather_api.get(requests, api_key=API_key, rate='informers', lat=lat, lon=lon)
+        forecast_data = data['forecast']
+        forecast = []
+        for date_forecast in forecast_data:
+            forecast.append({
+                'date': parse(date_forecast['date']),
+                'temp': date_forecast['temp_max']
+            })
+        return forecast
+
+
+class CityInfo:
+
+    def __init__(self, city):
+        self.city = city
+        self._forecast = YandexWeatherForecast()
+
+    def weather_forecast(self):
+        return self._forecast.get(self.city)
+
+
+def _main():
+    city_info = CityInfo('Mogilev')
+    forecast = city_info.weather_forecast()
+    pprint.pprint(forecast)
+
+
+if __name__ == '__main__':
+    _main()
