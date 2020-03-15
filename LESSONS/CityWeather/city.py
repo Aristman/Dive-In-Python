67bf9ff4aa81989ec10082db4d66e03d9e65194d
Aristman <armobila@yandex.ru>
2020-03-16 01:13:16 +0300

    Work WINDOWS 7 64

--- LESSONS/CityWeather/city.py
+++ LESSONS/CityWeather/city.py
@@ -1,41 +1,12 @@
 import pprint
-import requests
-from dateutil.parser import parse
-import yandex_weather_api
-import json
-
-
-class YandexWeatherForecast:
-
-    def get(self, city):
-        lat, lon = '53.894548', '30.330654'
-        API_key = '1539ef79-053b-4013-9bea-4b78ec83fe3d'
-        url = f'GET https://api.weather.yandex.ru/v1/informers?'
-        data = yandex_weather_api.get(requests, api_key=API_key, rate='informers', lat=lat, lon=lon)
-        forecast_data = data['forecast']
-        forecast = []
-        pprint.pprint(json.dumps(forecast_data, indent=4))
-        for date_forecast in forecast_data:
-            forecast.append({
-                'date': date_forecast['date'],
-                'temp': date_forecast['parts']['night']['temp_avg']
-            })
-        return forecast
-
-
-class CityInfo:
-
-    def __init__(self, city):
-        self.city = city
-        self._forecast = YandexWeatherForecast()
-
-    def weather_forecast(self):
-        return self._forecast.get(self.city)
+from Yandex_API import y_weather
 
 
 def _main():
-    city_info = CityInfo('Mogilev')
-    forecast = city_info.weather_forecast()
+    my_weather = y_weather.WeatherToSite()
+    forecast = my_weather.get_forecast('53.894548', '30.330654', '97c42aba-7868-4110-9f24-135609aaf07d')
+#    with open('farecast.txt', 'w') as f:
+#        f.writelines(forecast)
     pprint.pprint(forecast)
 
 
---
+++ Yandex_API/y_weather.py
@@ -0,0 +1,47 @@
+import requests
+
+
+class WeatherToSite():
+
+    def __init__(self):
+        self._condition = {
+            'clear': 'ясно',
+            'partly-cloudy': 'малооблачно',
+            'cloudy': 'облачно с прояснениями',
+            'overcast': 'пасмурно',
+            'partly-cloudy-and-light-rain': 'небольшой дождь',
+            'partly-cloudy-and-rain': "дождь",
+            'overcast-and-rain': 'сильный дождь',
+            'overcast-thunderstorms-with-rain': 'сильный дождь, гроза',
+            'cloudy-and-light-rain': 'небольшой дождь',
+            'overcast-and-light-rain': 'небольшой дождь',
+            'cloudy-and-rain': 'дождь',
+            'overcast-and-wet-snow': 'дождь со снегом',
+            'partly-cloudy-and-light-snow': 'небольшой снег',
+            'partly-cloudy-and-snow': 'снег',
+            'overcast-and-snow': 'снегопад',
+            'cloudy-and-light-snow': 'небольшой снег',
+            'overcast-and-light-snow': 'небольшой снег',
+            'cloudy-and-snow': 'снег'
+        }
+        pass
+
+    def get_forecast(self, lat, lon, API_key, lang='ru_RU'):
+        """Метод возвращает прогноз погоды в формаие JSON на ближайшие два периода
+
+        Входные параметры:
+
+        lat, lon - широта и долгота места для прогноза
+
+        API_key - ключ API для сервиса погоды Яндекс
+
+        lang - язык прогноза (см. справке Yandex)
+        """
+        url = f'https://api.weather.yandex.ru/v1/informers?'
+        head = {'X-Yandex-API-Key': API_key}
+        params = [
+            ('lat', lat),
+            ('lon', lon),
+            ('lang', lang)
+        ]
+        return requests.get(url, params=params, headers=head).json()
